c11664f3ba4c6a5ade6233aef1d66556c9fa379c
Mateo Pitarch <41348212+ZetuK@users.noreply.github.com>
2021-11-08 13:08:51 -0300

    Update CandidatoVO.java

--- src/modelo/vo/CandidatoVO.java
+++ src/modelo/vo/CandidatoVO.java
@@ -4,6 +4,10 @@ public class CandidatoVO {
 	private String nombre;
 	private String apellido;
 	private Integer DNI;
+	private Date fechaDeNacimiento;
+	private String nacionalidad;
+	private List<Strings> idiomas;
+	private List<Strings> intereses;
 	
 	public String getNombre() {
 		return nombre;
@@ -18,12 +22,36 @@ public class CandidatoVO {
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-	
+
 	public Integer getDNI() {
 		return DNI;
 	}
 	public void setDNI(Integer dNI) {
 		DNI = dNI;
 	}
-	
+
+	public Date getFechaDeNacimiento() {
+		return fechaDeNacimiento;
+	}
+	public void getFechaDeNacimiento(date fechaDeNacimiento) {
+		this.fechaDeNacimiento = fechaDeNacimiento;
+	}
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+	public List<Strings> getIdiomas() {
+		return idiomas;
+	}
+	public void setIdiomas(List<Strings> idiomas) {
+		this.idiomas = idiomas;
+	}
+	public List<Strings> getIntereses() {
+		return intereses;
+	}
+	public void setIntereses(List<Strings> intereses) {
+		this.intereses = intereses;
+	}
 }
