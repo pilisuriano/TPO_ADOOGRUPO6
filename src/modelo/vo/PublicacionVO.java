7f45eb358c710fd440b13d8a8f21e31f0987af9d
Mateo Pitarch <41348212+ZetuK@users.noreply.github.com>
2021-11-08 13:09:27 -0300

    Update PublicacionVO.java

--- src/modelo/vo/PublicacionVO.java
+++ src/modelo/vo/PublicacionVO.java
@@ -6,6 +6,13 @@ public class PublicacionVO {
 	private String titulo;
 	private Double sueldo;
 	private List<PostulacionVO> postulaciones;
+	private date fechaLimite;
+	private boolean postulacionActiva;
+	private String descripcion;
+	private bool modalidadFT;
+	private bool tipoDeTrabajoRemoto;
+	private String lugarTrabajo;
+	private Categoria categoria;
 	
 	public String getTitulo() {
 		return titulo;
@@ -25,4 +32,48 @@ public class PublicacionVO {
 	public void setPostulaciones(List<PostulacionVO> postulaciones) {
 		this.postulaciones = postulaciones;
 	}
+	public void setFechaLimite(date fechaLimite) {
+		this.setFechaLimite(); = fechaLimite;
+	}
+	public date getFechaLimite()  {
+		return fechaLimite;
+	}
+	public void activarPublicacion() {
+		this.postulacionActiva(); = true;
+	}
+	public void desactivarPublicacion() {
+		this.postulacionActiva(); = false;
+	}
+	public boolean getEstadoPublicacion() {
+		this.postulacionActiva();
+	}
+
+	public void setLugarTrabajo(string lugarTrabajo)
+	{
+		this.lugarTrabajo = lugarTrabajo;
+	}
+
+	public string getLugarTrabajo  {
+		return lugarTrabajo;
+	}
+
+	private void setCategoria (Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setTipoDeTrabajoRemoto(bool trabajoRemoto)
+	{
+		if (trabajoRemoto==true)
+			this.tipoDeTrabajoRemoto =true;
+		else
+			this.tipoDeTrabajoRemoto = false;
+	}
+	public bool getTipoDeTrabajoRemoto  {
+		this.tipoDeTrabajoRemoto();
+	}
+
 }
