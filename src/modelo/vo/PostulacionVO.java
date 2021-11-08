2ed190867f2fac836549260230a6cc0a1e38397c
Mateo Pitarch <41348212+ZetuK@users.noreply.github.com>
2021-11-08 19:07:39 -0300

    atributo remuneracion y metodos agregados

--- src/modelo/vo/PostulacionVO.java
+++ src/modelo/vo/PostulacionVO.java
@@ -5,6 +5,7 @@ public class PostulacionVO
 	private Integer mes;
 	private Integer anio;
 	private CandidatoVO candidato;
+	private Float remuneracion;
 	
 	public Integer getMes() {
 		return mes;
@@ -23,5 +24,12 @@ public class PostulacionVO
 	}
 	public void setCandidato(CandidatoVO candidato) {
 		this.candidato = candidato;
+	}
+	public void setRemuneracion(Integer remuneracion) {
+		this.remuneracion = remuneracion;
+	}
+
+	public float getRemuneracion() {
+		return remuneracion;
 	}
 }
